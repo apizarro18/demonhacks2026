1136ad60420c88e61ecf72ebd18b18c078b6a992
paulmurphyy <luappanda@gmail.com>
2026-02-28 16:22:57 -0600

    map fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import { 
-  MapContainer, 
-  TileLayer, 
-  useMap, 
-  Marker, 
-  Popup 
+import 'leaflet/dist/leaflet.css';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap
 } from 'react-leaflet';
+import { useEffect } from 'react';
+
+function FixMapSize() {
+  const map = useMap();
+  useEffect(() => {
+    map.invalidateSize();
+  }, [map]);
+  return null;
+}
 
 function App() {
   return (
-    <>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <FixMapSize />
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>Hello</Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
